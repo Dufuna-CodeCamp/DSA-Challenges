a99b5085e2cc4e95f124092619821211b885cd0b
EfeDaniel <EfeEjemudaro@gmail.com>
2021-09-10 07:33:42 +0100

    Add templete for challenge 2

---
+++ submissions/efeDaniel/challenge2-arrays/question1.js
@@ -0,0 +1,13 @@
+/**
+ * A multi line comment explaning the approach. 
+ * (An Alternative is Single line comments within the code)
+ */
+
+function solution(nums) {
+	// This is where i write my code and return the output
+}
+
+// Time Complexity: O(Yeah)
+// Space Complexity O(My God)
+
+
